25d77fd854d6425429919b66d634630063bcb3c5
Tyshchenko Oleksandr <o.tyshchenko19.03.1994@gmail.com>
2021-05-05 13:04:54 +0300

    edit version

--- script/Script1.js
+++ script/Script1.js
@@ -1,40 +1,32 @@
-"use strict";
-
-let slideIndex = 1;
-showSlides(slideIndex);
-
-function plusSlide() {
-    showSlides(slideIndex += 1);
-}
-
-function minusSlide() {
-    showSlides(slideIndex -= 1);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("slider-item");
-    let dots = document.getElementsByClassName("slider-dot_item");
-    if (n > slides.length) {
-        slideIndex = 1
-    }
-    if (n < 1) {
-        slideIndex = slides.length
-    }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-}
-
+let dots = document.getElementsByClassName('slider-dot_item');
+let arrows = document.getElementsByClassName('slider-btn');
+let slides = document.getElementsByClassName('slider-item');
+const getNumberOfActiveSlide = () => {numberOfActiveSlide = document.getElementsByClassName('active-slide')[0].dataset.slideNumber;}
+const removeActiveClass= () => {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].classList.remove('active-slide');
+    };
+};
+function changeSlide() {
+    removeActiveClass();
+    slides[this.dataset.dotNumber].classList.add('active-slide');
+};
+function arrow() {
+    getNumberOfActiveSlide();
+    removeActiveClass();
+    if (this == arrows[0]) {
+        if (numberOfActiveSlide == 0) {
+            numberOfActiveSlide = slides.length;
+        } slides[numberOfActiveSlide-1].classList.add('active-slide');
+    }   if (numberOfActiveSlide == slides.length - 1) {
+            numberOfActiveSlide = -1;
+        } slides[+numberOfActiveSlide + 1].classList.add('active-slide');
+};
+for (let i = 0; i < dots.length; i++) {
+    dots[i].addEventListener("click", changeSlide);
+    dots[i].setAttribute('data-dot-number', i);
+    slides[i].setAttribute('data-slide-number', i);
+};
+for (let j = 0; j < arrows.length; j++) {
+    arrows[j].addEventListener('click', arrow)
+};
